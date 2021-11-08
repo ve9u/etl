892ea719063f194ba792e86bb41bc6b07082fe49
Venu Gangireddy <venu.gangireddy@gmail.com>
2021-11-08 20:03:21 +0530

    Extract Data from MySQL

--- extract.py
+++ extract.py
@@ -1,21 +1,18 @@
 import pymysql
-import config
 
-def initalise_mysql():
-    """Initalises and returns a MySQL database based on config"""
-    return pymysql.connect(
-        host=config.MYSQL_HOST,
-        user=config.MYSQL_USERNAME,
-        password=config.MYSQL_PASSWORD,
-        db=config.MYSQL_DB)
+from config import MYSQL_DB, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USERNAME
 
-def extract_data(mysql_cursor):
-    """Given a cursor, Extracts data from MySQL classic product dataset
-    and returns all the tables with their data"""
-    genres = execute_mysql_query('select * from genres', mysql_cursor, 'fetchall')
-    movie_genres = execute_mysql_query('select * from movie_genres', mysql_cursor, 'fetchall')
-    movies = execute_mysql_query('select * from movies', mysql_cursor, 'fetchall')
-    ratings = execute_mysql_query('select * from ratings', mysql_cursor, 'fetchall')
-    users = execute_mysql_query('select * from users', mysql_cursor, 'fetchall')
-    tables = (genres, movie_genres, ratings, users, movies)
-    return tables
+def connect_db():
+    # Connect to the database
+    connection = pymysql.connect(host=MYSQL_HOST,
+                                user=MYSQL_USERNAME,
+                                password=MYSQL_PASSWORD,
+                                database=MYSQL_DB,
+                                cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+def extract_data(connection, sql):
+    with connection.cursor() as cursor:
+        # Read a single record
+        cursor.execute(sql)
+        return cursor
