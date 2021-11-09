992a962d784fdaf29990f8780e943f3eff388d17
Venu Gangireddy <venu.gangireddy@gmail.com>
2021-11-09 20:07:32 +0530

    Second Day

--- __main__.py
+++ __main__.py
@@ -1,4 +1,21 @@
+from extract import connect_db, extract_data
+
+
 def main():
     """main method starts a pipeline, extracts data,
     transforms it and loads it into a mongo client"""
-    print ("Coding start from here")
+    # print ("Coding start from here")
+
+    # Connect to the database
+    mysql = connect_db()
+
+    #Extract all prdoucts informations
+    cur = extract_data(mysql, "SELECT * FROM products")
+
+    for i in range(0, cur.rowcount):
+        row = cur.fetchone()
+        print(row['productCode'], row['productName'])
+
+    print("dummy")
+
+main()
